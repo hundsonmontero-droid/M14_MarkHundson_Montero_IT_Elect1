391590746e4999b6ef96a14b6dfdc1da645ca74b
Montero_MarkHundson <hundsonmontero@gmail.com>
2025-09-22 11:20:03 +0800

    Full Commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import ChatScreen from './ChatScreen';
+import React from "react";
+import CommentInput from './CommentInput.js'
+
+export default function App() {
+  return ( <>
+<ChatScreen />
+<CommentInput/>
+</>
+)
+}
+
